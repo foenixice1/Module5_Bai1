e17f6008205cff212c0b54d44b5694fa6a18c78a
Nguyễn Hoài Nam <83760339+foenixice1@users.noreply.github.com>
2021-09-17 17:16:20 +0700

    BT3

---
+++ BT1/src/main.js
@@ -0,0 +1,11 @@
+// @ts-ignore
+function fibonacci(number) {
+    if (number <= 1) {
+        return 1;
+    }
+    return fibonacci(number - 1) + fibonacci(number - 2);
+}
+var number = 10;
+for (var i = 0; i < number; i++) {
+    console.log(fibonacci(i));
+}
